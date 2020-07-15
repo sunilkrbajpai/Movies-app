eaf5c26b4f140a236a6b144bf1758d1e3ac0159d
sunil <sunilkumarbajpai.skb@gmail.com>
2020-07-15 14:54:17 +0530

    subscribe to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
 
-function App() {
+
+class App extends React.Component {
+
+  componentDidMount(){
+
+    const {store}=this.props;
+    store.subscribe(()=>{
+      console.log('updated');
+      this.forceUpdate();
+    })
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+    });
+  }
+  render(){
+  const movies=this.props.store.getState();
+
   return (
     <div className="App">
-      Project setupdsfdsfdsfdsfds
+      
+      <Navbar/>
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>  
+          <div className="tab">Favorites</div>
+        </div>
+
+        <div className="list">
+          {movies.map((movie,index)=>(
+            <MovieCard movie={movie} key={`movies-${index}`}/>
+          ))}
+        </div>
+      </div>
+
+
     </div>
   );
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store=createStore(movies);
+// console.log(store);
+
+// //action
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{name:'Syperman'}]
+// });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
