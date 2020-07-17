2fec88b5f2b9d3a348beaf0599378395a5a6ff89
sunil <sunilkumarbajpai.skb@gmail.com>
2020-07-18 00:11:08 +0530

    changing the state to add fav

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
+import { addMovies } from '../actions';
 
 
 class App extends React.Component {
@@ -13,13 +14,18 @@ class App extends React.Component {
       console.log('updated');
       this.forceUpdate();
     })
-    store.dispatch({
-      type:'ADD_MOVIES',
-      movies:data
-    });
+    store.dispatch(addMovies(data));
+
+    // store.dispatch({
+    //   type:'ADD_MOVIES',
+    //   movies:data
+    // });
+
   }
   render(){
-  const movies=this.props.store.getState();
+  // const movies=this.props.store.getState();
+  const {list}=this.props.store.getState();
+
 
   return (
     <div className="App">
@@ -32,7 +38,7 @@ class App extends React.Component {
         </div>
 
         <div className="list">
-          {movies.map((movie,index)=>(
+          {list.map((movie,index)=>(
             <MovieCard movie={movie} key={`movies-${index}`}/>
           ))}
         </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,18 @@
-export default function movies(state=[],action)
+
+import {ADD_MOVIES} from '../actions';
+
+const initialMoviesState={
+    list:[],
+    favorites:[]
+}
+
+export default function movies(state=initialMoviesState,action)
 {
-    if(action.type==='ADD_MOVIES'){
-        return action.movies;
+    if(action.type===ADD_MOVIES){
+        return {
+            ...state,
+            list:action.movies
+        };
     }
     return state;
 }
