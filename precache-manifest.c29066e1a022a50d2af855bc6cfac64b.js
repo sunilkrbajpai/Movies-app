472a1c11dc54cd7832eb569544e2159c2489b97d
sunil <sunilkumarbajpai.skb@gmail.com>
2020-08-09 01:13:43 +0530

    Updates

---
+++ precache-manifest.c29066e1a022a50d2af855bc6cfac64b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda5c73e16dea5d243aaf4769a8ae9a9",
+    "url": "/Movies-app/index.html"
+  },
+  {
+    "revision": "3ec976ae2d11fa98bd31",
+    "url": "/Movies-app/static/css/main.57fbc47d.chunk.css"
+  },
+  {
+    "revision": "385bb1d416e80d9e1763",
+    "url": "/Movies-app/static/js/2.e75bf07f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movies-app/static/js/2.e75bf07f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec976ae2d11fa98bd31",
+    "url": "/Movies-app/static/js/main.a6f83bc4.chunk.js"
+  },
+  {
+    "revision": "017285116487b71363c2",
+    "url": "/Movies-app/static/js/runtime-main.eb54e0b7.js"
+  }
+]);
